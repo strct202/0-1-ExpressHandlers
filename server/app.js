c51aea8965a26aff56de9b7b7a3e4431b6c15da7
strct202 <af101033@gmail.com>
2022-10-18 20:20:50 +0000

    Update app.js and package-lock.json

--- server/app.js
+++ server/app.js
@@ -18,13 +18,33 @@ const {
   getSongBySongId,
   addSongByAlbumId,
   editSongBySongId,
-  deleteSongBySongId
-} = require('./data');
+  deleteSongBySongId,
+} = require("./data");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Your code here
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
+
+app.use(express.json());
+
+function requireJSON() {
+  return (req, res, next) => {
+    if (req.headers["content-type"] !== "application/json") {
+      res.status(400).send("Error: Request requires JSON format");
+    } else {
+      next();
+    }
+  };
+}
+
+app.post("/", requireJSON(), (req, res, next) => {
+  let content = req.body;
+  console.log(JSON.stringify(content));
+
+  res.send("Recieved request");
+  next();
+});
